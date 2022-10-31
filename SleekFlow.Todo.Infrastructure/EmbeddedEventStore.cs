088ec048dce2cdb8696c06ffddf199a070adf3eb
Stephen Tung <stktung@gmail.com>
2022-10-31 16:02:55 +0800

    Added in-memory embedded eventstore DB and make sure it works

---
+++ SleekFlow.Todo.Infrastructure/EmbeddedEventStore.cs
@@ -0,0 +1,36 @@
+﻿using EventStore.ClientAPI;
+using EventStore.ClientAPI.Embedded;
+using EventStore.Core;
+using Microsoft.Diagnostics.Tracing.Parsers.Tpl;
+using System.Diagnostics;
+
+namespace SleekFlow.Todo.Infrastructure
+{
+    public class EmbeddedEventStore
+    {
+        private static readonly TimeSpan TimeToStop = TimeSpan.FromSeconds(5);
+
+        private ClusterVNode _node;
+
+        public EmbeddedEventStore()
+        {
+            var nodeBuilder = EmbeddedVNodeBuilder
+                .AsSingleNode()
+                .OnDefaultEndpoints()
+                .RunInMemory();
+
+            _node = nodeBuilder.Build();
+            _node.StartAsync(true).Wait();
+
+            Connection = EmbeddedEventStoreConnection.Create(_node);
+        }
+
+        public IEventStoreConnection Connection { get; }
+
+        public void Dispose()
+        {
+            Connection?.Close();
+            _node.StopAsync(TimeToStop).Wait();
+        }
+    }
+}
